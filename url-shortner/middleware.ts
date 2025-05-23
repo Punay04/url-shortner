8810b561c2ee4bcfd3376ab2345a705e85c55e95
Punay kukreja <kukrejapunay04@gmail.com>
2025-05-23 15:35:14 +0530

    update middleware.ts

--- url-shortner/middleware.ts
+++ url-shortner/middleware.ts
@@ -1,23 +1,24 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher([
-  "/login(.*)",
   "/",
+  "/login(.*)",
   "/register",
-  "/api(.*)", // Add API routes to public routes
+  "/api(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
-    await auth.protect();
+    const { userId } = await auth();
+
+    if (!userId) {
+      // Not signed in, redirect to login
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
   }
 });
-
+  
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!_next|.*\\..*).*)"],
 };
